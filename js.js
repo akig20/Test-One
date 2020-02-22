020fef2caea79e5c91b4e296fbba4c47be2838d2
akig20 <61227409+akig20@users.noreply.github.com>
2020-02-22 21:30:27 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,23 +1,28 @@
-$(document).ready(function(){
-    $("#loginButton").click(function(){
-        var username = $("#loginName").val().trim();
-        var password = $("#loginPass").val().trim();
 
-        if( username != "" && password != "" ){
-            $.ajax({
-                url:'http://webbred2.utb.hb.se/~fewe/api/api.php?data=students',
-                type:'post',
-                data:{username:username,password:password},
-                success:function(response){
-                    var msg = "";
-                    if(response == 1){
-                        window.location.href = "html/quiz.html";
-                    }else{
-                        msg = "Ogiltig Användarnamn eller Lösenord!";
-                    }
-                    $("#message").html(msg);
-                }
-            });
+$("#loginForm").submit(function(event)
+{
+    event.preventDefault();
+
+    $.ajax({
+        url: "http://webbred2.utb.hb.se/~fewe/api/api.php?data=students",
+        type: "GET",
+        dataType:"json",
+        data:({
+            username: $('#username').val(),
+            password: $('#password').val()
+        }),
+        success: function(result)
+        {
+            if(result && result.auth_token.length>1) // you should do your checking here
+            {
+                window.location.href = 'http://www.google.com/'; //just to show that it went through
+            }
+            else
+            {
+                $('#result').empty().addClass('error')
+                    .append('Something is wrong.');
+            }
         }
     });
-});
+    return false;
+  })
